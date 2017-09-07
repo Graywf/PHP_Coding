5ea70d86b7b0d27be91aaa0847b2c9642bc5cb17
Brad <barnesba@Brads-iMac.lan>
2017-09-06 23:48:11 -0400

    Start of PHP webpage
    Beginning Code

---
+++ php_s1v4/inc/functions.php
@@ -0,0 +1,26 @@
+<?php
+	function get_item_html($id,$item) {
+		output = "<li><a herf='#'><img src='" 
+			. $item["img"] . "' alt='" 
+			. $item["title"] . "' />"
+			. "<p>View Details</p>"
+			. "</a></li>";
+		return $output;	
+	}
+	
+	function array_category($catalog,$category) {
+		if (category == null) {
+			return array_keys($category);
+		}
+		$output = array();
+		
+		foreach ($catalog as $id => $item) {
+			if (strtolower($category) == strtolower($item["category"])) {
+			$output[] = $id;
+		}
+	}
+	
+	return $output;
+	}
+	
+	
